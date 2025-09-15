25ce1fb56ddca6c3f1741d882603dfe8f894ddef
igormarusic <igor.marusich@medi.report>
2025-09-15 21:51:37 +0200

    feat: module federation

---
+++ apps/audit/src/app/remote-entry/nx-welcome.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nx-welcome',
+  imports: [CommonModule],
+  template: `Audit component`,
+  styles: [],
+  encapsulation: ViewEncapsulation.None,
+  standalone: true,
+})
+export class NxWelcome {}
---
+++ apps/dashboard/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { NxWelcome } from './nx-welcome';
+import { Route } from '@angular/router';
+
+export const appRoutes: Route[] = [
+  {
+    path: 'audit',
+    loadChildren: () => import('audit/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
+    path: '',
+    component: NxWelcome,
+  },
+];
